3295c35209c95d4291a3ac7549ccc64fb83710e9
myway00 <junior0614@naver.com>
2022-12-09 14:31:38 +0900

    REFACTOR : JOIN FETCH JPQL

--- src/main/java/server/twalk/PvP/repository/PvpMatchRepository.java
+++ src/main/java/server/twalk/PvP/repository/PvpMatchRepository.java
@@ -14,7 +14,7 @@ public interface PvpMatchRepository extends JpaRepository<PvpMatch, Long> {
     List<PvpMatch> findByReceiverOrderByCreatedAtDesc(Member member);
 
     @Query(
-            "select i from PvpMatch inner join fetch i.requester r" +
+            "select i from PvpMatch inner join fetch i.requester r " +
                     "i where i IN (:pvpMatch) order by i.createdAt desc"
     )
     List<PvpMatch> findByPvpMatches(@Param("pvpMatch") List<PvpMatch> PvpMatches);
