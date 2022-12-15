9b0f2e16f75f7934aaa92338ef81b3e38d968c63
myway00 <junior0614@naver.com>
2022-12-16 08:41:34 +0900

    fix

--- src/main/java/server/twalk/Member/repository/MemberRepository.java
+++ src/main/java/server/twalk/Member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package server.twalk.Member.repository;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,12 +9,14 @@ import server.twalk.Walking.entity.LatLonPair;
 
 import java.util.Optional;
 
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(
             "select m from Member m where m.id = (:id)"
     )
     Optional<Member> findByIdCustom(@Param("id")Long id);
 
+    @Modifying
     @Query("UPDATE Member m SET m.latLonPair = :l WHERE m.id = :id")
     void latLonPairUpdate(@Param("l") LatLonPair l, @Param("id") Long id);
 
